ba7d8a76f8a9f9dddd4af0549d55e9332a08d878
Victor Fonseca <86002152+ofonsecavictor@users.noreply.github.com>
2022-03-20 21:43:39 -0300

    .

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View, Text, ScrollView } from 'react-native';
+import {View, ScrollView } from 'react-native';
 import {Header} from '../../components/Header'
 import {Categories} from '../../components/Categories'
 import {Baseboard} from '../../components/ Baseboard'
+import { styles } from './styles';
 export function HomeScreen() {
     return(
-        <View style={{flex:1, backgroundColor: '#fff'}}>
+        <View style={styles.container}>
             <Header/>
             <ScrollView style={{flex:1, backgroundColor: '#fff'}}>
             <Categories/>
