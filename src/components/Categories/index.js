b77e1e5afad50bba8869651743e290113fb963e1
Victor Fonseca <86002152+ofonsecavictor@users.noreply.github.com>
2022-03-20 21:41:37 -0300

    Update index.js

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -10,42 +10,33 @@ export function Categories () {
   const navigation = useNavigation();
     const [categories, setCategories] = useState ([])
     const [posts, setPosts] = useState ([])
-    const [media, setMedia] = useState ([])
 
-    useEffect(() => {
+    useEffect((id) => {
       api
-        .get('/wp-json/wp/v2/categories/')
+        .get('/wp-json/wp/v2/categories')
         .then(response => setCategories(response.data))
         .catch(err => {
           console.error('ops! ocorreu um erro' + err);
         });
     }, []);
 
-      useEffect(() => {
+      useEffect((id) => {
         api
-          .get('/wp-json/wp/v2/posts')
+          .get('/wp-json/wp/v2/posts?_embed')
           .then(response => setPosts(response.data))
           .catch(err => {
             console.error('ops! ocorreu um erro' + err);
           });
       }, []);
 
-      useEffect(() => {
-        api
-          .get('/wp-json/wp/v2/media/?include=5053')
-          .then(response => setMedia(response.data))
-          .catch(err => {
-            console.error('ops! ocorreu um erro' + err);
-          });
-      }, []);
-
     return (
+
         <ScrollView style={{width: '100%', height: '100%'}}>
            {categories.map((category, id) => {
           return (
            <View>
         <View style={styles.titlecontainer}>
-             <Text  style={styles.title}>{category.name}</Text>
+             <Text style={styles.title}>{category.name}</Text>
              <TouchableOpacity>
              <Text>VER MAIS</Text>
              </TouchableOpacity>
@@ -62,13 +53,10 @@ export function Categories () {
                     content:posts.content.rendered})
                     }>
 
-{media.map((media) => {
-          return (
-                <Image style={{width: 235, height:103, borderTopRightRadius: 12,borderTopLeftRadius: 12}}source={{ uri: media.media_details.sizes.medium.source_url || '' }}
-
+                <Image style={{width: 235, height:103, borderTopRightRadius: 12,borderTopLeftRadius: 12}}
+                source={{uri:posts._embedded['wp:featuredmedia'][0].source_url}}
                 />
-                );
-              })}
+
                     <Text style={styles.subtitle}>{posts.title.rendered}</Text>
                         <HTML numberOfLines={4} html={posts.excerpt.rendered} style={styles.description}/>
                         <TouchableOpacity>
@@ -83,5 +71,7 @@ export function Categories () {
                                       })}
 
         </ScrollView>
-    )
+
+
+)
 }
